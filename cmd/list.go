b1b68e79ea8144841a53fabe7c03da0ddb9bdfec
Maxr1998 <max.rumpf1998@gmail.com>
2025-04-06 20:32:55 +0200

    Use url history to show more details in list

--- cmd/list.go
+++ cmd/list.go
@@ -6,6 +6,8 @@ import (
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/maxr1998/s3share-cli/conf"
 	"github.com/maxr1998/s3share-cli/core"
+	"github.com/maxr1998/s3share-cli/store"
+	"github.com/maxr1998/s3share-cli/util"
 	"github.com/spf13/cobra"
 	"strings"
 )
@@ -23,34 +25,57 @@ var listCmd = &cobra.Command{
 		ctx := cmd.Context()
 		files, err := core.ListUploadedFiles(ctx)
 		cobra.CheckErr(err)
+		history := util.ReadHistory()
 
 		tableWriter := table.NewWriter()
 		tableWriter.SetOutputMirror(cmd.OutOrStdout())
 		tableWriter.SuppressEmptyColumns()
 
-		tableWriter.AppendHeader(table.Row{"File ID", "Size", "Last Modified", "Warnings"})
+		tableWriter.AppendHeader(table.Row{"File ID", "Name", "Size", "Checksum", "Last Modified", "Warnings"})
 		totalSize := int64(0)
 		for _, file := range files {
-			totalSize += file.Metadata.Size
+			// Initialize metadata with defaults
+			var decryptedMetadata = &store.DecryptedMetadata{
+				Size: file.Metadata.Size,
+			}
+			if historyUrl := history[file.FileId]; historyUrl != nil {
+				if metadata, err := file.Metadata.Decrypt(history[file.FileId].Key); err == nil {
+					decryptedMetadata = metadata
+				}
+			}
+
+			totalSize += decryptedMetadata.Size
+			var checksum string
+			if decryptedMetadata.Checksum != nil {
+				checksum = fmt.Sprintf("%x", decryptedMetadata.Checksum)
+			} else {
+				checksum = ""
+			}
 			var lastModified string
 			if file.Exists {
 				lastModified = humanize.Time(file.LastModified)
 			} else {
 				lastModified = "N/A"
 			}
+			var warning string
+			if !file.Exists {
+				warning = "Missing data"
+			} else if decryptedMetadata.Size != file.ObjectSize {
+				warning = "Size mismatch, data may be corrupt"
+			}
+
 			row := table.Row{
 				file.FileId,
-				humanize.IBytes(uint64(file.Metadata.Size)),
+				decryptedMetadata.Name,
+				humanize.IBytes(uint64(decryptedMetadata.Size)),
+				checksum,
 				lastModified,
+				warning,
 			}
-			if !file.Exists {
-				row = append(row, "Missing data")
-			} else if file.Metadata.Size != file.ObjectSize {
-				row = append(row, "Size mismatch, data may be corrupt")
-			}
+
 			tableWriter.AppendRow(row)
 		}
-		tableWriter.AppendFooter(table.Row{"TOTAL", humanize.IBytes(uint64(totalSize)), "N/A"})
+		tableWriter.AppendFooter(table.Row{"TOTAL", "", humanize.IBytes(uint64(totalSize)), "N/A", "N/A"})
 		tableWriter.Render()
 	},
 }
--- util/history.go
+++ util/history.go
@@ -1,9 +1,11 @@
 package util
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/maxr1998/s3share-cli/conf"
 	"os"
+	"strings"
 )
 
 func AddToHistory(url ShareableUrl) {
@@ -13,4 +15,25 @@ func AddToHistory(url ShareableUrl) {
 	}
 	defer CloseFileOrExit(historyFile)
 	_, _ = fmt.Fprintf(historyFile, "%v\n", url)
+}
+
+func ReadHistory() map[string]*ShareableUrl {
+	historyFile, err := os.Open(conf.HistoryFileLocation)
+	if err != nil {
+		return nil
+	}
+	defer CloseFileOrExit(historyFile)
+
+	urls := make(map[string]*ShareableUrl)
+	scanner := bufio.NewScanner(historyFile)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if url, err := ParseUrl(line); err == nil {
+			urls[url.FileId] = url
+		} else {
+			fmt.Println(line)
+		}
+	}
+
+	return urls
 }
