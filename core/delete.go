322828a5aa157bb53e6888c2f14155aac21824fc
Maxr1998 <max.rumpf1998@gmail.com>
2026-05-02 19:13:50 +0200

    Fix broken delete command
    A check was incorrectly inverted during the last refactor.

--- core/delete.go
+++ core/delete.go
@@ -9,7 +9,7 @@ import (
 
 func DeleteFile(ctx context.Context, fileId string) error {
 	// Check if the file exists before enqueuing it for deletion
-	if store.HasFileMetadata(ctx, fileId) {
+	if !store.HasFileMetadata(ctx, fileId) {
 		return errors.New("file not found")
 	}
 	if err := store.DeleteDataObject(ctx, fileId); err != nil {
